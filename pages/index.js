dda019d88a263e0743d604281a26120ca5877dcb
Al Mondragón <al.drago@live.com>
2021-05-08 23:49:35 -0500

    Finish desktop design and functionality

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,24 @@
+import { Disclosure, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/solid'
 import Head from 'next/head'
+import faqData from '../services/faqData'
 
 export default function Home() {
   return (
     <div
-      className='overflow-hidden text-[12px]'
+      className='overflow-hidden text-[14px] text-[#1d1e35]'
       style={{
         background:
           'linear-gradient(180deg, rgba(175,103,233,1) 0%, rgba(101,101,231,1) 100%)',
       }}
     >
       <Head>
-        <title>FAQ Accordion</title>
+        <title>FAQ Accordion Card</title>
         <link rel='icon' href='/favicon.png' />
       </Head>
 
       <main className='flex min-h-screen items-center justify-center'>
-        <div className='bg-white max-w-4xl h-[500px] rounded-3xl grid grid-cols-2 relative shadow-2xl'>
+        <div className='bg-white w-[900px] h-[500px] rounded-3xl grid grid-cols-2 relative shadow-2xl mx-20'>
           <img
             src='/illustration-box-desktop.svg'
             alt='Box illustration desktop'
@@ -33,8 +36,50 @@ export default function Home() {
               className='absolute bottom-10 -left-40 transform scale-150 h-full'
             />
           </div>
-          <div>
-            <h1 className='text-3xl font-bold mt-20 mb-10 mx-10'>FAQ</h1>
+          <div className='px-14'>
+            <h1 className='text-[32.5px] font-bold mt-14 mb-5'>FAQ</h1>
+
+            {faqData.map(({ question, answer }, index) => (
+              <Disclosure key={index}>
+                {({ open }) => (
+                  <>
+                    <Disclosure.Button className='w-full flex pt-4 pb-3 border-b-[1px] border-[#e7e7e9] justify-between'>
+                      <p
+                        className={`text-[#4a4b5e] hover:text-[#f47c57] ${
+                          open
+                            ? 'font-bold text-[#1d1e35] hover:text-[#1d1e35]'
+                            : ''
+                        }`}
+                      >
+                        {question}
+                      </p>
+                      <ChevronDownIcon
+                        className={`h-5 w-5 text-[#f47c57] ${
+                          open ? 'transform rotate-180' : ''
+                        }`}
+                      />
+                    </Disclosure.Button>
+
+                    <Transition
+                      show={open}
+                      enter='transition duration-100 ease-out'
+                      enterFrom='transform scale-95 opacity-0'
+                      enterTo='transform scale-100 opacity-100'
+                      leave='transition duration-75 ease-out'
+                      leaveFrom='transform scale-100 opacity-100'
+                      leaveTo='transform scale-95 opacity-0'
+                    >
+                      <Disclosure.Panel
+                        static
+                        className='mt-3 mb-1 text-[12px] text-[#787887]'
+                      >
+                        {answer}
+                      </Disclosure.Panel>
+                    </Transition>
+                  </>
+                )}
+              </Disclosure>
+            ))}
           </div>
         </div>
       </main>
